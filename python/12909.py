cdd07ccbbef92be477bb4cfac5c92dec892ba9a4
jjunier <yyt1186@gmail.com>
2026-07-31 15:37:34 +0900

    [prgms] Lv.2 올바른 괄호 (12909)

--- python/12909.py
+++ python/12909.py
@@ -1,4 +1,11 @@
 def solution(s):
+    """
+    Args:
+        s(List): '('와 ')'로만 이루어진 문자열 리스트
+    
+    Returns:
+        (Boolean): s 리스트에 담긴 괄호 문자열들의 열리고 닫힘이 올바른 형태인지에 대한 참/거짓
+    """
     if s[0] == ')' or s[-1] == '(' or len(s) % 2 == 1:
         return False
     
