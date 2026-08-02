3e13f516cb0206e97698c8d482925ebf4cbbac29
jjunier <yyt1186@gmail.com>
2026-08-02 11:06:03 +0900

    [prgms] Lv.2 다리를 지나는 트럭 (42583)

--- python/42583.py
+++ python/42583.py
@@ -11,27 +11,30 @@ def solution(bridge_length: int, weight: int, truck_weights: List[int]) -> int:
     Returns:
         (Int): 모든 트럭이 다리를 건너는 데에 걸리는 최소 시간(초)
     """
-    bridge = deque([0] * bridge_length)
-    trucks = deque(truck_weights)
-    current_weight = 0
-    time = 0
+    waiting_trucks = deque(truck_weights)
+    bridge_on_trucks = deque()
     
-    while bridge:
+    time = 0
+    current_weight = 0
+    
+    while waiting_trucks or bridge_on_trucks:
         time += 1
-        current_weight -= bridge.popleft()
         
-        if trucks:
-            if current_weight + trucks[0] <= weight:
-                truck = trucks.popleft()
-                bridge.append(truck)
-                current_weight += truck
+        # 다리를 모두 건넌 트럭 제거하기
+        if bridge_on_trucks and time - bridge_on_trucks[0][1] == bridge_length:
+            truck_weight, entered_time = bridge_on_trucks.popleft()
+            current_weight -= truck_weight
             
-            else:
-                bridge.append(0)
+        # 다음 트럭이 다리에 진입할 수 있는지 검사하기
+        if waiting_trucks:
+            next_truck = waiting_trucks[0]
+            
+            if(
+                len(bridge_on_trucks) < bridge_length 
+               and current_weight + next_truck <= weight
+              ):
+                next_truck = waiting_trucks.popleft()
+                bridge_on_trucks.append((next_truck, time))
+                current_weight += next_truck
                 
-        if not trucks and current_weight == 0:
-            break
-            
     return time
-            
-
