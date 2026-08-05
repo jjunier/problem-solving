5d807d4141144696ffbd86f1c603b6f87bbc2241
jjunier <yyt1186@gmail.com>
2026-08-05 16:36:02 +0900

    [prgms] Lv.2 디스크 컨트롤러 (42627)

--- python/42627.py
+++ python/42627.py
@@ -1,25 +1,44 @@
 import heapq
 
 def solution(jobs):
-    jobs = sorted([(req, duration, idx) for idx, (req, duration) in enumerate(jobs)])
-    heap = []
-    time = 0
-    i = 0
-    total = 0
-    n = len(jobs)
+    """
+    Args:
+        jobs(Matrix): [요청 시간, 소요 시간] 쌍을 갖는 2차원 리스트
+
+    Returns:
+        (Int): 모든 요청 작업의 반환 시간에 대한 평균
+    """
+    jobs_with_number = []
     
-    while i < n or heap:
-        while i < n and jobs[i][0] <= time:
-            req, duration, idx = jobs[i]
-            heapq.heappush(heap, (duration, req, idx))
-            i += 1
+    for job_number, (request_time, duration) in enumerate(jobs):
+        jobs_with_number.append((request_time, duration, job_number))
+        
+    jobs_with_number.sort()
+    waiting_heap = []
+    current_time = 0
+    job_index = 0
+    completed_count = 0
+    total_turnaround = 0
+    
+    while completed_count < len(jobs):
+        
+        while (job_index < len(jobs_with_number) 
+               and jobs_with_number[job_index][0] <= current_time
+              ):
+            request_time, duration, job_number = jobs_with_number[job_index]
             
-        if heap:
-            duration, req, idx = heapq.heappop(heap)
-            time += duration
-            total += time - req
+            heapq.heappush(waiting_heap, (duration, request_time, job_number))
+            
+            job_index += 1
+            
+        if waiting_heap:
+            duration, request_time, job_number = heapq.heappop(waiting_heap)
+            
+            current_time += duration
+            total_turnaround += current_time - request_time
+            completed_count += 1
+            
         else:
-            time = jobs[i][0]
+            current_time = jobs_with_number[job_index][0]
             
-    return total // n
-            
+    return total_turnaround // len(jobs)
