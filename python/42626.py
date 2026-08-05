c0ff0c786500e218facecb8833a6630789485278
jjunier <yyt1186@gmail.com>
2026-08-05 14:22:41 +0900

    [prgms] Lv.2 더 맵게 (42626)

--- python/42626.py
+++ python/42626.py
@@ -1,14 +1,26 @@
 import heapq
 
 def solution(scoville, K):
+    """
+    Args:
+        scoville(List): 모든 음식의 스코빌 지수가 담긴 정수형 리스트
+        K(Int): 음식의 목표 스코빌 지수
+
+    Returns:
+        count(Int): 모든 음식을 목표 스코빌 지수(K) 이상으로 만들기 위한 음식 섞기 최소 횟수 
+    """
     heapq.heapify(scoville)
     count = 0
     
-    while len(scoville) > 1 and scoville[0] < K:
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
         mixed = first + (second * 2)
+        
         heapq.heappush(scoville, mixed)
         count += 1
         
-    return count if scoville[0] >= K else - 1
+    return count
