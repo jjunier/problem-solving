a951a1245c7a927ba7d8ea142b7395af1a8404fc
jjunier <yyt1186@gmail.com>
2026-08-10 21:07:42 +0900

    [prgms] Lv.1 K번째수 (42748)

--- python/42748.py
+++ python/42748.py
@@ -1,4 +1,12 @@
 def solution(array, commands):
+    """
+    Args:
+        array(List): 정렬할 숫자가 담긴 리스트
+        commands(Matrix): i번째부터 j번째 숫자를 자르고 정렬한 후, k번째 숫자가 담긴 이차원 리스트
+
+    Returns:
+        (List): commands의 결과들이 담긴 정수형 리스트
+    """
     answer = []
     
     for i, j, k in commands:
