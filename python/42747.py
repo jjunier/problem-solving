e401e686d61350980e90745ee48098ec892ddbf7
jjunier <yyt1186@gmail.com>
2026-08-10 23:40:58 +0900

    [prgms] Lv.2 H-Index (42747)

--- python/42747.py
+++ python/42747.py
@@ -1,8 +1,22 @@
 def solution(citations):
+    """
+    Args:
+        citations(List): 어떤 과학자가 발표한 갹 논문별 인용 횟수
+
+    Returns:
+        h_index(Int): 연구자가 발표한 논문의 양과 질을 동시에 평가하는 수치 지표인 h_index
+    """
     citations.sort(reverse=True)
     
-    for i, citation in enumerate(citations):
-        if i + 1 > citation:
-            return i
+    h_index = 0
+    
+    for index, citation in enumerate(citations):
+        paper_count = index + 1
         
-    return len(citations)
+        if citation >= paper_count:
+            h_index = paper_count
+            
+        else:
+            break
+            
+    return h_index
