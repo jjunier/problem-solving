09643dce13bb918e5f19084794ba7157ca22f56c
jjunier <yyt1186@gmail.com>
2026-08-10 21:36:53 +0900

    [prgms] Lv.2 가장 큰 수 (42746)

--- python/42746.py
+++ python/42746.py
@@ -1,5 +1,21 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
+    """
+    Args:
+        numbers: 이어 붙여 만들 수 있는 0 또는 양의 정수가 담긴 리스트
     
-    return str(int(''.join(numbers)))
+    Returns:
+        (Int): 이어 붙여 만든 가장 큰 정수
+    """
+    numbers = list(map(str, numbers))
+    
+    numbers.sort(
+        key=lambda x : x * 4,
+        reverse=True
+    )
+    
+    answer = ''.join(numbers)
+    
+    if answer[0] == '0':
+        return '0'
+    
+    return answer
