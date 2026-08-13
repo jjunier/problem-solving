013363010cc13b81068b6dd59fa541d383b577f1
jjunier <yyt1186@gmail.com>
2026-08-13 20:19:09 +0900

    [prgms] Lv.2 모음 사전 (84512)

--- python/84512.py
+++ python/84512.py
@@ -1,9 +1,22 @@
+from itertools import product
+
+
 def solution(word):
-    answer = 0
+    """
+    Args:
+        word(String): 알파벳 모음(A, E, I, O, U)으로 이루어진 문자열
+
+    Returns
+        (Int): 문자열이 정해진 규칙에서의 사전 속 몇 번째에 위치하는 지 나타내는 정수
+    """
     vowels = ['A', 'E', 'I', 'O', 'U']
-    weights = [781, 156, 31, 6, 1]
-    
-    for i, ch in enumerate(word):
-        answer += vowels.index(ch) * weights[i] + 1
-    
-    return answer
+    words = []
+
+    for length in range(1, 6):
+        for combination in product(vowels, repeat=length):
+            current_word = ''.join(combination)
+            words.append(current_word)
+
+    words.sort()
+
+    return words.index(word) + 1
