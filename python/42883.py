cb22e77fc7e5c73a6d907c843432ca6e7cf1b01d
jjunier <yyt1186@gmail.com>
2026-08-16 19:10:35 +0900

    [prgms] Lv.2 큰 수 만들기 (42883)

--- python/42883.py
+++ python/42883.py
@@ -1,4 +1,12 @@
 def solution(number, k):
+    """
+    Args:
+        numbers(String): 문자열 형식의 숫자
+        k(Int): numbers에서 제거할 숫자 갯수
+
+    Returns:
+        (String): numbers에서 k개 만큼 제거한 숫자 중에서 가장 큰 숫자를 문자열 형태로 변환
+    """
     stack = []
     
     for digit in number:
