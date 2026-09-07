2817dac844c629976f7dbc4ac1c6c3d6039ead49
jjunier <yyt1186@gmail.com>
2026-09-07 18:28:31 +0900

    [prgms] Lv.3 가장 먼 노드 (49189)

---
+++ python/49189.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def solution(n, vertex):
+    """
+    Args:
+        n(Int): 노드의 갯수
+        vertex(Matrix): 각 노드별로 이어진 간선의 정보가 담긴 이차원 리스트
+
+    Returns:
+        (Int): 1번 노드로부터 가장 멀리 떨어진 노드의 갯수
+    """
+    graph = [[] for _ in range(n + 1)]
+    
+    for a, b in vertex:
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    distance = [-1] * (n + 1)
+    distance[1] = 0
+    
+    queue = deque([1])
+    
+    while queue:
+        current = queue.popleft()
+        
+        for next_node in graph[current]:
+            if distance[next_node] == -1:
+                distance[next_node] = distance[current] + 1
+                queue.append(next_node)
+                
+    max_distance = max(distance)
+    
+    return distance.count(max_distance)
