6ad75a9e738f68392cc25ca3d6c9e7962f8d9615
jjunier <yyt1186@gmail.com>
2026-09-10 23:58:30 +0900

    [prgms] Lv.3 정수 삼각형 (43105)

--- python/43105.py
+++ python/43105.py
@@ -1,4 +1,11 @@
 def solution(triangle):
+    """
+    Args:
+        triangle(Matrix): 상각형의 정보가 담긴 이차원 리스트
+
+    Returns:
+        (Int): 꼭대기에서 바닥으로 오른쪽 또는 왼쪽 대각선으 방향으로 거쳐 가는데 만들 수 있는 최댓값
+    """
     dp = [row[:] for row in triangle]
 
     for i in range(1, len(dp)):
