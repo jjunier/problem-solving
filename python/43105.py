27d804f9c76920a3ad3703a353f09cd322706469
jjunier <yyt1186@gmail.com>
2026-09-11 16:41:57 +0900

    [prgms] Lv.3 정수 삼각형 (43105)

--- python/43105.py
+++ python/43105.py
@@ -4,17 +4,19 @@ def solution(triangle):
         triangle(Matrix): 상각형의 정보가 담긴 이차원 리스트
 
     Returns:
-        (Int): 꼭대기에서 바닥으로 오른쪽 또는 왼쪽 대각선으 방향으로 거쳐 가는데 만들 수 있는 최댓값
+        (Int): 꼭대기에서 바닥으로 오른쪽 또는 왼쪽 대각선의 방향으로 거쳐 가는데 만들 수 있는 최댓값
     """
-    dp = [row[:] for row in triangle]
-
-    for i in range(1, len(dp)):
-        for j in range(len(dp[i])):
+    triangle_info = [row[:] for row in triangle]
+    
+    for i in range(1, len(triangle_info)):
+        for j in range(len(triangle_info[i])):
             if j == 0:
-                dp[i][j] += dp[i - 1][j]
-            elif j == len(dp[i]) - 1:
-                dp[i][j] += dp[i - 1][j - 1]
+                triangle_info[i][j] += triangle_info[i - 1][j]
+                
+            elif j == i:
+                triangle_info[i][j] += triangle_info[i - 1][j - 1]
+                
             else:
-                dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j])
-
-    return max(dp[-1])
+                triangle_info[i][j] += max(triangle_info[i - 1][j], triangle_info[i - 1][j - 1])
+                
+    return max(triangle_info[-1])
